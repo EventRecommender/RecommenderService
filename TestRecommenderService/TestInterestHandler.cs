a5fb2b2e9bfae27b82491538819e37846c22ebd7
Mads Christian Bruun Nielsen <mcbn19@student.aau.dk>
2022-12-02 12:19:33 +0100

    Restructure to include test-project

---
+++ TestRecommenderService/TestInterestHandler.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RecommenderService.Classes;
+
+namespace TestRecommenderService
+{
+	[TestClass]
+	public class TestInterestHandler
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
