15616f0a82ff6125073f9558c671bbc844633ee0
Mads Christian Bruun Nielsen <mcbn19@student.aau.dk>
2022-12-05 11:19:28 +0100

    GetSimilarInterests: Added SQL to find similar users

--- RecommenderService/Classes/InterestHandler.cs
+++ RecommenderService/Classes/InterestHandler.cs
@@ -27,7 +27,9 @@ namespace RecommenderService.Classes
 			connection.Open();
 
 			//Check if user exist
-			string SQLstatement = $"SELECT COUNT(*) WHERE UserID = {user_ID}";
+			string SQLstatement = $"SELECT COUNT(*)" +
+										$" FROM interest" +
+										$" WHERE UserID = {user_ID}";
 			SqlCommand command = new SqlCommand(SQLstatement, connection);
 			SqlDataReader dataReader = command.ExecuteReader();
 
@@ -60,7 +62,10 @@ namespace RecommenderService.Classes
 			}
 			//Check for user done
 
-			//Calculate other interests
+			//Calculate interests
+
+			List<string> interest = GetSimilarInterests(initial_types);
+
 
 			SQLstatement = "";
 			command = new SqlCommand(SQLstatement, connection);
@@ -76,6 +81,41 @@ namespace RecommenderService.Classes
 			connection.Close();
 
 			return ErrorStatus.Success;
+		}
+
+		public List<string> GetSimilarInterests(List<string> initial_types)
+		{
+			connection.Open();
+
+			int val1 = 15;
+			int val2 = 40; // used to stop outliers.
+
+			string SQLstatement =	$"SELECT DISTINCT UserID " +
+									$"FROM (SELECT UserID, COUNT(UserID) as count" +
+									$"			FROM Interest" +
+									$"			WHERE TypeID IN ('{initial_types[0]}', '{initial_types[1]}', '{initial_types[2]}', '{initial_types[3]}')" +
+									$"			AND TypeID IN" +
+									$"				(SELECT * " +
+									$"						FROM Interest" +
+									$"						WHERE Value BETWEEN {val1} AND {val2})) " +
+									$"WHERE count = 4"; // "count" is the number of types which are the same as those in initial_types.
+
+
+
+			SqlCommand command = new SqlCommand(SQLstatement, connection);
+			SqlDataReader dataReader = command.ExecuteReader();
+
+			List<int> similarUsers = new List<int>();
+
+			while (dataReader.Read())
+			{
+				similarUsers.Add((int)dataReader[0]);
+			}
+
+			command.Dispose();
+
+			connection.Close();
+			return default;
 		}
 
 	}
